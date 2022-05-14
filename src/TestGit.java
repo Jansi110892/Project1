b5d4a7814f7f2c2ff55c3cebaa38179be379471b
unknown <jansi110892>
2022-05-14 17:24:12 +0530

    Done

---
+++ src/TestGit.java
@@ -0,0 +1,23 @@
+
+public class TestGit {
+
+	public void add() {
+		System.out.println("Addition");
+	}
+	public void sub() {
+		System.out.println("Subtraction");
+	}
+	public static void main(String[] args) {
+		
+		TestGit tg = new TestGit();
+		tg.add();
+		tg.sub();
+	
+		
+	
+		
+		
+
+	}
+
+}
